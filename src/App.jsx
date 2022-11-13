625da0bbfaebb5d449a4605d66b7a70a65d70ea1
Memo Chou <memochou1993@gmail.com>
2022-11-13 23:15:29 +0800

    Update main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const sites = [
+    //
+  ]
 
   return (
-    <div className="App">
+    <>
+      <h1>epoch.tw</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {
+            sites.map((site, i) => (
+              <li key={i}>
+                <a href={site.link} target="_blank">
+                  { site.name }
+                </a>
+              </li>
+            ))
+          }
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
